53d062eedf59968f2e78893792519793e89fc012
teklakh <127101552+teklakh@users.noreply.github.com>
2024-03-10 23:56:13 +0400

    Add files via upload

---
+++ src/Application.jsx
@@ -0,0 +1,12 @@
+function Application () {
+    const variable = "hello world"
+    return (
+        <h1>
+            {
+                variable
+            }
+        </h1>
+    );
+}
+
+export default Application;
